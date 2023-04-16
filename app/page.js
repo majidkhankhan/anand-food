fcb22acb5a4026dae6a21b25e1b1b8cd50e01105
Majid Khan <79964050+majidkhankhan@users.noreply.github.com>
2023-04-16 10:39:48 +0530

    chnage data

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>hello</h1>
+      <h1>hello i am the majid</h1>
     </main>
   );
 }
